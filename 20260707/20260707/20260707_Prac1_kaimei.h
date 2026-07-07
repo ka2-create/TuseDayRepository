0e5dd0d296851a291a712bf51710d0be5590d271
ka2-create <kaimei042655@icloud.com>
2026-07-07 16:21:36 +0900

    0707

---
+++ 20260707/20260707/20260707_Prac1_kaimei.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+//定数
+const int INDEX = 10;
+const int MAX = 100;
+const int MIN = 0;
+/// <summary>
+///配列追加関数
+/// /// </summary>
+/// /// <param name="ary">追加する関数</param>
+/// /// <param name="size">配列の大きさ</param>
+void AddArray(int ary[], int size);
+
+int InputCheck(int min, int max);
