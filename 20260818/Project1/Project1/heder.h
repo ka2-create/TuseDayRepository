7cf792049a7be234d692ab0a8c1cacfb69440c92
ka2-create <kaimei042655@icloud.com>
2026-08-18 16:19:31 +0900

    0818sinntyokubunn???

---
+++ 20260818/Project1/Project1/heder.h
@@ -0,0 +1,26 @@
+#pragma once
+#include<iostream>
+
+using namespace std;
+
+void heal(int* hp)
+{
+
+}
+void damage(int* hp)
+{
+
+}
+
+int main()
+{
+
+int hp = 100;
+int damage = 20;
+int heal = 30;
+
+cout << hp << endl;
+
+return 0;
+
+}
